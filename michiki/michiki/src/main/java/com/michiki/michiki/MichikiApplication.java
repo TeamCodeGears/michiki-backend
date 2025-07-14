34fe0bb7502e2102b14a9a16efe32a67a16a5481
TsunaguMahou <hammizzang@gmail.com>
2025-07-14 16:28:19 +0900

    initial commit

---
+++ michiki/michiki/src/main/java/com/michiki/michiki/MichikiApplication.java
@@ -0,0 +1,13 @@
+package com.michiki.michiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichikiApplication.class, args);
+	}
+
+}
---
+++ michiki/michiki/src/test/java/com/michiki/michiki/MichikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michiki.michiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
