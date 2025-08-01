963fca8d3b79ee1efa6e388e531d0c6f24514e15
namomi <xicodey@gmail.com>
2025-08-01 14:24:49 +0900

    [Fix] 쓸데없는 필드 삭제

--- michiki/src/main/java/com/michiki/michiki/place/dto/PlaceReorderRequestDto.java
+++ michiki/src/main/java/com/michiki/michiki/place/dto/PlaceReorderRequestDto.java
@@ -6,14 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Data
 public class PlaceReorderRequestDto {
 
-    @NotNull
-    private LocalDate travelDate;
     @NotEmpty
     private List<PlaceOrder> places;
 
