9eabf3f13b764e52ba5a3d7bfcff333950c93784
Sena_No_OshiP <124870298+Haeheum@users.noreply.github.com>
2025-08-02 16:01:56 +0900

    feat: plans 조회 기능 완료

--- michiki/src/main/java/com/michiki/michiki/plan/repository/PlanRepository.java
+++ michiki/src/main/java/com/michiki/michiki/plan/repository/PlanRepository.java
@@ -2,14 +2,22 @@ package com.michiki.michiki.plan.repository;
 
 import com.michiki.michiki.plan.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
-public interface PlanRepository extends JpaRepository<Plan, Long>{
+public interface PlanRepository extends JpaRepository<Plan, Long> {
 
-
-    Optional<Plan> findById(Long aLong);
+    @Query("select p from Plan p join p.memberPlans mp where mp.member.memberId" +
+            " = :memberId and p.startDate >= :jan1 and p.startDate <= :dec31")
+    List<Plan> findAllByMemberIdAndStartDateInYear(
+            @Param("memberId") Long memberId,
+            @Param("jan1") LocalDate jan1,
+            @Param("dec31") LocalDate dec31
+    );
 }
 
